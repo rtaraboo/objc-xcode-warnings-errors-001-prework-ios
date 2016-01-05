1d04a29f48b0aa208872cfdc64507f34135b6c73
rtaraboo <rtarabocchia@gmail.com>
2016-01-05 13:35:17 -0500

    Warnings and Errors complete

--- objc-xcode-warnings-errors/FISAppDelegate.m
+++ objc-xcode-warnings-errors/FISAppDelegate.m
@@ -9,40 +9,43 @@
     
     NSString *unused = @"This variable generates a warning because it is unused.";
     
-//    NSLog(@"%@", unused);
+    NSLog(@"%@", unused);
     
-//    NSInteger *i = 0;
-//    NSLog(@"i: %li", i);
+    NSInteger i = 12;
+    NSLog(@"i: %li", i);
     
-//    NSInteger x = i + 1;
-//    NSLog(@"x: %li", x);
+    NSInteger x = i + 1;
+    NSLog(@"x: %li", x);
     
     NSLog(@"Anything after the return statement will not get executed.");
+    
+    
+    NSString *message = @"Even though they don't belong here, the compiler won't actually complain about string literals or primitives defined outside of a method body (which is held by  curly braces {...} ), but...";
+    
+    NSInteger j = 0;
+    BOOL itIsKnownKhaleesi = YES;
+    
+    NSLog(@"...any statements containing function calls, operations, or method calls will produce errors.");
+    
+    
+    
+    
+    NSLog(@"%@", message);
+    
+    
+    
+    j++;
+    
+    itIsKnownKhaleesi = NO;
+    
+    
+    NSString *notLocal = @"Which means the variables above, while permitted, can't be used in the way that you intend.";
     
     return YES; // this line ends the method
     
     NSLog(@"Take note that this line doesn't print to the console.");
 }
 
-//NSString *message = @"Even though they don't belong here, the compiler won't actually complain about string literals or primitives defined outside of a method body (which is held by  curly braces {...} ), but...";
 
-//NSInteger j = 0;
-//BOOL itIsKnownKhaleesi = YES;
-
-//NSLog(@"...any statements containing function calls, operations, or method calls will produce errors.");
-
-
-
-
-//NSLog(@"%@", message);
-
-
-
-//j++;
-
-//itIsKnownKhaleesi = NO;
-
-
-//NSString *notLocal = [NSString stringWithString:@"Which means the variables above, while permitted, can't be used in the way that you intend."];
 
 @end
